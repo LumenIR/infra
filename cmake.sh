3082556d34c8e6de8e785b558d69f2ade4c5693b
Sergey Gvozdarev <gvozdarev.sa@gmail.com>
2018-11-20 11:43:30 +0300

    Update scripts

--- cmake.sh
+++ cmake.sh
@@ -1,14 +1,17 @@
 
 #local PROJECTS="clang;lld;lldb;"
 PROJECTS="clang;llr;"
+CC=gcc
+CXX=g++
 
-cmake ../repos/llvm -G Ninja -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_BUILD_TYPE=Debug \
+cmake ../repos/llvm -G Ninja -DCMAKE_C_COMPILER=${CC} -DCMAKE_CXX_COMPILER=${CXX} -DCMAKE_BUILD_TYPE=Debug \
     -DLLVM_EXTERNAL_PROJECTS=${PROJECTS} \
     -DLLVM_ENABLE_PROJECTS=${PROJECTS} \
-    -DCMAKE_C_FLAGS="-gsplit-dwarf" \
-    -DCMAKE_CXX_FLAGS="-gsplit-dwarf" \
+    -DCMAKE_C_FLAGS="-fuse-ld=gold -gsplit-dwarf" \
+    -DCMAKE_CXX_FLAGS="-fuse-ld=gold -gsplit-dwarf" \
     -DLLVM_PARALLEL_LINK_JOBS=2 \
-    -DLLVM_TARGETS_TO_BUILD="LumenIR;ARM;X86" \
+    -DLLVM_TARGETS_TO_BUILD="X86;ARM" \
+    -DLLVM_EXPERIMENTAL_TARGETS_TO_BUILD="RISCV" \
     -DBUILD_SHARED_LIBS=1
 
 
